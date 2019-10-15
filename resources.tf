5ed44980945b1172b5b4a7163ed50ed701a222f4
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-15 22:06:21 +0200

    Added initial setup for adding heroku apps.

---
+++ resources.tf
@@ -0,0 +1,3 @@
+resource "random_id" "app" {
+  byte_length = 4
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "app_prefix" {
+  default = "pgr301-ex"
+}
+
+variable "pipeline_name" {
+  default = "pgr301-pipeline"
+}
