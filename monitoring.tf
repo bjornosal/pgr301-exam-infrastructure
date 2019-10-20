71768e9a1bfc09f788d346a034a1e929d5e7cb6f
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-20 15:01:03 +0200

    Updated website url to be pinger.

--- monitoring.tf
+++ monitoring.tf
@@ -1,14 +1,14 @@
 resource "statuscake_test" "stage" {
-  website_name  = "exam-stage"
-  website_url   = "${heroku_app.staging.web_url}"
+  website_name  = "exam-stage-ping"
+  website_url   = "${heroku_app.staging.web_url}/ping"
   test_type     = "HTTP"
   contact_group = ["Examinators", "Examee"]
 
 }
 
 resource "statuscake_test" "prod" {
-  website_name  = "app-prod"
-  website_url   = "${heroku_app.production.web_url}"
+  website_name  = "exam-prod-ping"
+  website_url   = "${heroku_app.production.web_url}/ping"
   test_type     = "HTTP"
   contact_group = ["Examinators", "Examee"]
 
