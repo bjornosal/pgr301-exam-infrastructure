0fa34dcbc5b25c66658f91b0ce0ad0200047c168
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-20 15:05:13 +0200

    Fixed endpoint

--- monitoring.tf
+++ monitoring.tf
@@ -1,6 +1,6 @@
 resource "statuscake_test" "stage" {
   website_name  = "exam-stage-ping"
-  website_url   = "${heroku_app.staging.web_url}/ping"
+  website_url   = "${heroku_app.staging.web_url}ping"
   test_type     = "HTTP"
   contact_group = ["Examinators", "Examee"]
 
@@ -8,7 +8,7 @@ resource "statuscake_test" "stage" {
 
 resource "statuscake_test" "prod" {
   website_name  = "exam-prod-ping"
-  website_url   = "${heroku_app.production.web_url}/ping"
+  website_url   = "${heroku_app.production.web_url}ping"
   test_type     = "HTTP"
   contact_group = ["Examinators", "Examee"]
 
