268cbaaf48754bff4b9ac99d2d890a45969a0662
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-28 09:26:18 +0100

    Fixed variable names.

--- pipeline.tf
+++ pipeline.tf
@@ -5,7 +5,7 @@ resource "heroku_app" "development" {
 }
 
 resource "heroku_addon" "db_development" {
-  app  = "${heroku_app.developement.name}"
+  app  = "${heroku_app.development.name}"
   plan = "heroku-postgresql:hobby-dev"
 }
 
@@ -36,8 +36,8 @@ resource "heroku_pipeline" "exam" {
 }
 
 # Couple apps to different pipeline stages
-resource "heroku_pipeline_coupling" "developement" {
-  app      = "${heroku_app.developement.name}"
+resource "heroku_pipeline_coupling" "development" {
+  app      = "${heroku_app.development.name}"
   pipeline = "${heroku_pipeline.exam.id}"
   stage    = "development"
 }
