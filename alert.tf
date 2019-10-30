5e581e682102101da516a6c397a9e59ce2e881b6
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-30 19:30:39 +0100

    Updated opsgenie teams and users

--- alert.tf
+++ alert.tf
@@ -3,6 +3,15 @@ provider "opsgenie" {
   api_url = "api.eu.opsgenie.com"
 }
 
+#Users
+resource "opsgenie_user" "superuser" {
+  username  = "super@some.domain.com"
+  full_name = "Super Usersson"
+  role      = "Owner"
+  locale    = "no_NO"
+  timezone  = "Europe/Oslo"
+}
+
 resource "opsgenie_user" "user1" {
   username  = "user1@some.domain.com"
   full_name = "User1 Usersson"
@@ -11,20 +20,80 @@ resource "opsgenie_user" "user1" {
   timezone  = "Europe/Oslo"
 }
 
-resource "opsgenie_user_contact" "sms" {
+resource "opsgenie_user" "user2" {
+  username  = "user2@some.domain.com"
+  full_name = "User2 Usersson"
+  role      = "User"
+  locale    = "no_NO"
+  timezone  = "Europe/Oslo"
+}
+#End users
+
+#User contact
+resource "opsgenie_user_contact" "user1_sms" {
   username = "${opsgenie_user.user1.username}"
-  to      = "47-91761030"
-  method  = "sms"
+  to       = "47-91761030"
+  method   = "sms"
 }
 
-resource "opsgenie_user_contact" "email" {
+resource "opsgenie_user_contact" "user1_email" {
   username = "${opsgenie_user.user1.username}"
-  to      = "bjorn.olav.salvesen@gmail.com"
-  method  = "email"
+  to       = "bjorn.olav.salvesen@gmail.com"
+  method   = "email"
 }
 
-resource "opsgenie_user_contact" "voice" {
+resource "opsgenie_user_contact" "user1_voice" {
   username = "${opsgenie_user.user1.username}"
-  to      = "47-91761030"
-  method  = "voice"
+  to       = "47-91761030"
+  method   = "voice"
+}
+
+resource "opsgenie_user_contact" "user2_email" {
+  username = "${opsgenie_user.user2.username}"
+  to       = "user2@second.work.email.com"
+  method   = "email"
+}
+
+resource "opsgenie_user_contact" "user2_voice" {
+  username = "${opsgenie_user.user2.username}"
+  to       = "47-99999999"
+  method   = "voice"
+}
+
+resource "opsgenie_user_contact" "user2_sms" {
+  username = "${opsgenie_user.user2.username}"
+  to       = "47-99999999"
+  method   = "sms"
+}
+
+resource "opsgenie_user_contact" "super" {
+  username = "${opsgenie_user.superuser.username}"
+  to       = "47-99999999"
+  method   = "voice"
+}
+#End user contact
+
+# Teams
+resource "opsgenie_team" "tier_one" {
+  name        = "Tier 1"
+  description = "This team deals with the basic shit"
+
+  member {
+    id   = "${opsgenie_user.user1.id}"
+    role = "user"
+  }
+  member {
+    id   = "${opsgenie_user.user2.id}"
+    role = "user"
+  }
+}
+
+resource "opsgenie_team" "tier_one" {
+  name        = "Superadmins"
+  description = "This team fixes all the things that has Tier 1 can't handle"
+
+  member {
+    id   = "${opsgenie_user.superuser.id}"
+    role = "admin"
+  }
 }
