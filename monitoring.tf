d3810691ba03d60e128719e5516f5e8e3a635f49
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-30 18:23:47 +0100

    Updated names and contact groups.

--- monitoring.tf
+++ monitoring.tf
@@ -1,5 +1,5 @@
 resource "statuscake_test" "stage-http" {
-  website_name  = "exam-stage-ping"
+  website_name  = "exam-stage-ping-controller-http"
   website_url   = "${heroku_app.staging.web_url}ping"
   test_type     = "HTTP"
   contact_group = ["Tier 1"]
@@ -12,15 +12,15 @@ resource "statuscake_test" "stage-ping" {
 }
 
 resource "statuscake_test" "prod-http" {
-  website_name  = "exam-prod-ping"
+  website_name  = "exam-prod-ping-controller-http"
   website_url   = "${heroku_app.production.web_url}ping"
   test_type     = "HTTP"
-  contact_group = ["Tier 1"]
+  contact_group = ["Tier 2"]
 }
 
 resource "statuscake_test" "prod-ping" {
   website_name  = "exam-prod-ping"
   website_url   = "${heroku_app.production.web_url}ping"
   test_type     = "PING"
-  contact_group = ["Tier 1"]
+  contact_group = ["Tier 2"]
 }
