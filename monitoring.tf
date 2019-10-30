b6daad84161d0d83511521471ab80f42558fa534
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-30 18:15:52 +0100

    Added two more statuscake tests

--- monitoring.tf
+++ monitoring.tf
@@ -1,13 +1,26 @@
-resource "statuscake_test" "stage" {
+resource "statuscake_test" "stage-http" {
   website_name  = "exam-stage-ping"
   website_url   = "${heroku_app.staging.web_url}ping"
   test_type     = "HTTP"
-  contact_group = ["Examinators", "Examee"]
+  contact_group = ["Tier 1"]
+}
+resource "statuscake_test" "stage-ping" {
+  website_name  = "exam-stage-ping"
+  website_url   = "${heroku_app.staging.web_url}/devices"
+  test_type     = "PING"
+  contact_group = ["Tier 1"]
 }
 
-resource "statuscake_test" "prod" {
+resource "statuscake_test" "prod-http" {
   website_name  = "exam-prod-ping"
   website_url   = "${heroku_app.production.web_url}ping"
   test_type     = "HTTP"
-  contact_group = ["Examinators", "Examee"]
+  contact_group = ["Tier 1"]
+}
+
+resource "statuscake_test" "prod-ping" {
+  website_name  = "exam-prod-ping"
+  website_url   = "${heroku_app.production.web_url}ping"
+  test_type     = "PING"
+  contact_group = ["Tier 1"]
 }
