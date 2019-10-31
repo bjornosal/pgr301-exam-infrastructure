907aa33428080617995fb4b9efdef3711669f38e
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-31 18:31:55 +0100

    Update statuscake

--- monitoring.tf
+++ monitoring.tf
@@ -6,9 +6,10 @@ resource "statuscake_test" "stage-http" {
 }
 resource "statuscake_test" "stage-ping" {
   website_name  = "exam-stage-ping"
-  website_url   = "${heroku_app.staging.web_url}/devices"
+  website_url   = "${heroku_app.staging.web_url}devices"
   test_type     = "PING"
   contact_group = ["Tier 1"]
+  timeout       = 60
 }
 
 resource "statuscake_test" "prod-http" {
@@ -20,7 +21,8 @@ resource "statuscake_test" "prod-http" {
 
 resource "statuscake_test" "prod-ping" {
   website_name  = "exam-prod-ping"
-  website_url   = "${heroku_app.production.web_url}ping"
+  website_url   = "${heroku_app.production.web_url}devices"
   test_type     = "PING"
   contact_group = ["Tier 2"]
+  timeout       = 60
 }
