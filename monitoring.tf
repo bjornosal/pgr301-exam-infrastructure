4fcb8e1d97791fd51861811312863b0c24650fd9
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-31 18:36:28 +0100

    Removed ping from statuscake

--- monitoring.tf
+++ monitoring.tf
@@ -4,13 +4,15 @@ resource "statuscake_test" "stage-http" {
   test_type     = "HTTP"
   contact_group = ["Tier 1"]
 }
-resource "statuscake_test" "stage-ping" {
-  website_name  = "exam-stage-ping"
-  website_url   = "${heroku_app.staging.web_url}devices"
-  test_type     = "PING"
-  contact_group = ["Tier 1"]
-  timeout       = 60
-}
+
+# Disabled by Heroku apparently.
+# resource "statuscake_test" "stage-ping" {
+#   website_name  = "exam-stage-ping"
+#   website_url   = "${heroku_app.staging.web_url}devices"
+#   test_type     = "PING"
+#   contact_group = ["Tier 1"]
+#   timeout       = 60
+# }
 
 resource "statuscake_test" "prod-http" {
   website_name  = "exam-prod-ping-controller-http"
@@ -18,11 +20,11 @@ resource "statuscake_test" "prod-http" {
   test_type     = "HTTP"
   contact_group = ["Tier 2"]
 }
-
-resource "statuscake_test" "prod-ping" {
-  website_name  = "exam-prod-ping"
-  website_url   = "${heroku_app.production.web_url}devices"
-  test_type     = "PING"
-  contact_group = ["Tier 2"]
-  timeout       = 60
-}
+# Disabled by Heroku apparently.
+# resource "statuscake_test" "prod-ping" {
+#   website_name  = "exam-prod-ping"
+#   website_url   = "${heroku_app.production.web_url}devices"
+#   test_type     = "PING"
+#   contact_group = ["Tier 2"]
+#   timeout       = 60
+# }
