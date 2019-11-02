ac03fff36c70a0c209ce4b784efaecfaef1655e0
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-11-02 12:17:05 +0100

    Changed provider bucket name

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pgr301bucket"
-    key    = "examinator/terraform.tfstate"
+    key    = "kristiania-pgr301/terraform.tfstate"
     region = "eu-north-1"
   }
 }
